eaac3d4ea05a1907192b119bd830ec5da73f4626
daniel stuut <daniel@stuut.com.au>
2018-04-10 12:18:13 +1000

    remove script

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,3 @@
 import React from "react"
 
 export default () => <div>Hello world!</div>
-
-<script src="https://code.jquery.com/jquery-1.11.2.js"></script>
-
-  <!-- Make sure to include Nelify's authentiation library -->
-  <!-- Also available via npm as netlify-auth-providers -->
-  <script src="https://unpkg.com/netlify-auth-providers"></script>
-
-  <script>
-    $(function() {
-      $("#login").on("click", function(e) {
-        e.preventDefault();
-        var authenticator = new netlify.default ({});
-        authenticator.authenticate({provider:"github", scope: "user"}, function(err, data) {
-          if (err) {
-            return $("#output").text("Error Authenticating with GitHub: " + err);
-          }
-          $("#output").text("Authenticated with GitHub. Access Token: " + data.token);
-        });
-      });
-    });
-  </script>
