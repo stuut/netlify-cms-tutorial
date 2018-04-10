f4ae74a12a1b8090f1f0ff13efe077906dfe4ca3
daniel stuut <daniel@stuut.com.au>
2018-04-10 16:15:36 +1000

    blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  plugins: [`gatsby-plugin-netlify-cms`],
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/blog`,
+        name: 'blog',
+      },
+    },
+    `gatsby-transformer-remark`,
+
+
+
+  ],
 };
