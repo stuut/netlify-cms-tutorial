2bce4cd163b89daa46eed982d0198670d7e7c6a5
daniel stuut <daniel@stuut.com.au>
2018-04-11 15:31:45 +1000

    new blog

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,19 +7,21 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
 
   return graphql(`
     {
-      allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            frontmatter {
-              path
-            }
+    allMarkdownRemark(limit: 1000) {
+      edges {
+        node {
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            tags
+            templateKey
           }
         }
       }
     }
+  }
   `).then(result => {
     if (result.errors) {
       return Promise.reject(result.errors);
